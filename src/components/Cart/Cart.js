23087d0ec4be235eade8a5c49bb5004a92932e78
Alex Maior <alexmaior@yahoo.com>
2021-09-01 14:04:28 +0300

    Added the remove button functionailty in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem({ id });
+    cartCtx.removeItem(id);
   };
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
